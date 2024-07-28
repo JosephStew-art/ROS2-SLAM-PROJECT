aeadcc3b34052d3a9a5e80e5dff3f4e50ddaf639
Joseph <joseph.nomiddlename@gmail.com>
2024-07-28 18:03:41 +0200

    node:toggle_gpio altered the node to receive UDP packets with keyboard instructions from local computer as a remote controller with basic movement

--- ros2_ws/install/my_robot_controller/lib/python3.10/site-packages/my_robot_controller/toggle_gpio.py
+++ ros2_ws/install/my_robot_controller/lib/python3.10/site-packages/my_robot_controller/toggle_gpio.py
@@ -1,55 +1,92 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from time import sleep
 import RPi.GPIO as GPIO
+import socket
 
-class GPIOToggleNode(Node):
+class RobotControlNode(Node):
     def __init__(self):
-        super().__init__('gpio_toggle_node')
-        
+        super().__init__('robot_control_node')
+
         # GPIO pin setup
         self.gpio_pins_1 = [17, 27]
         self.gpio_pins_2 = [5, 6]
-        
+
+        self.gpio_pins_1_1 = 17
+        self.gpio_pins_1_2 = 27
+        self.gpio_pins_2_1 = 5
+        self.gpio_pins_2_2 = 6
+
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        
+
         for pin in self.gpio_pins_1 + self.gpio_pins_2:
             GPIO.setup(pin, GPIO.OUT)
 
-        self.state = False
+        # Set initial state to stop
+        self.stop()
 
-        # Set initial state
-        self.set_gpio_state(self.state)
+        # Setup UDP socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(('0.0.0.0', 12345))
 
-        # Create a timer to toggle the GPIO pins
-        self.timer = self.create_timer(2.0, self.toggle_gpio)
+        # Create a timer to check for incoming UDP messages
+        self.timer = self.create_timer(0.1, self.check_udp)
 
-    def set_gpio_state(self, state):
-        if state:
-            GPIO.output(self.gpio_pins_1, GPIO.HIGH)
-            GPIO.output(self.gpio_pins_2, GPIO.LOW)
-        else:
-            GPIO.output(self.gpio_pins_1, GPIO.LOW)
-            GPIO.output(self.gpio_pins_2, GPIO.HIGH)
+    def check_udp(self):
+        try:
+            data, addr = self.sock.recvfrom(1024)
+            command = data.decode('utf-8')
+            self.get_logger().info(f'Received command: {command}')
+            self.command_callback(command)
+        except socket.error as e:
+            self.get_logger().error(f'Socket error: {e}')
 
-    def toggle_gpio(self):
-        # Set both sets of GPIO pins to LOW for 1 second
+
+    def command_callback(self, command):
+        if command == 'w':
+            self.forward()
+        elif command == 's':
+            self.backward()
+        elif command == 'a':
+            self.rotate_counterclockwise()
+        elif command == 'd':
+            self.rotate_clockwise()
+        elif command == 'x':
+            self.stop()
+
+    def forward(self):
+        GPIO.output(self.gpio_pins_1_1, GPIO.HIGH)
+        GPIO.output(self.gpio_pins_1_2, GPIO.HIGH)
+        GPIO.output(self.gpio_pins_2_1, GPIO.LOW)
+        GPIO.output(self.gpio_pins_2_2, GPIO.LOW)
+
+    def backward(self):
+        GPIO.output(self.gpio_pins_1_1, GPIO.LOW)
+        GPIO.output(self.gpio_pins_1_2, GPIO.LOW)
+        GPIO.output(self.gpio_pins_2_1, GPIO.HIGH)
+        GPIO.output(self.gpio_pins_2_2, GPIO.HIGH)
+
+    def rotate_clockwise(self):
+        GPIO.output(self.gpio_pins_1_1, GPIO.LOW)
+        GPIO.output(self.gpio_pins_1_2, GPIO.HIGH)
+        GPIO.output(self.gpio_pins_2_1, GPIO.HIGH)
+        GPIO.output(self.gpio_pins_2_2, GPIO.LOW)
+
+    def rotate_counterclockwise(self):
+        GPIO.output(self.gpio_pins_1_1, GPIO.HIGH)
+        GPIO.output(self.gpio_pins_1_2, GPIO.LOW)
+        GPIO.output(self.gpio_pins_2_1, GPIO.LOW)
+        GPIO.output(self.gpio_pins_2_2, GPIO.HIGH)
+
+    def stop(self):
         GPIO.output(self.gpio_pins_1, GPIO.LOW)
         GPIO.output(self.gpio_pins_2, GPIO.LOW)
-        sleep(1)
-
-        # Toggle state
-        self.state = not self.state
-
-        # Set GPIO pins based on the new state
-        self.set_gpio_state(self.state)
-        self.get_logger().info(f'State: {"HIGH" if self.state else "LOW"}')
 
 def main(args=None):
     rclpy.init(args=args)
-    node = GPIOToggleNode()
+    node = RobotControlNode()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
